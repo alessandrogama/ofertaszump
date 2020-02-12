4a2052f0f9a0ef454bdc20f16c18620886868ee8
Alessandro Olavo Gama <alessandro.gama@aracaju.se.gov.br>
2020-02-12 16:14:20 -0300

    Initial commit

---
+++ lib/utils/flutkart.dart
@@ -0,0 +1,19 @@
+class Flutkart {
+  static const String name = "Ofertas Zump";
+  static const String store = "Melhores Ofertas\n para você";
+  static const String wt1 = "BEM-VINDO";
+  static const String wc1 =
+      "Ofertas diárias\n para você";
+  static const String wt2 = "O QUE É O OFERTAS Zump";
+  static const String wc2 =
+      "E um app onde selecionamos\n diversas ofertas com o melhor preço";
+  static const String wt3 = "MELHOR OFERTA";
+  static const String wc3 =
+      "Melhores preços\n você encontra aqui!";
+  static const String wt4 = "VERIFIQUE O PRODUTO";
+  static const String wc4 =
+      "Escolha já seu produto\n e adquira com o menor preço";
+  static const String skip = "PULAR";
+  static const String next = "PRÓXIMO";
+  static const String gotIt = "IR";
+}
